d2ea676d1771be2717a087e7fd5731a8b13f6b11
Muhammad Usman Siddiqui <usman.us.mus@gmail.com>
2023-12-14 21:56:27 +0300

    Day 3, part 1 solved

---
+++ 3/3_1.py
@@ -0,0 +1,30 @@
+def adjacent_symbol_present(a,b,n,lines):
+    for i in range(a-1, a+2):
+        for j in range(b-1, b+1+n):
+            if i<0 or i>=len(lines) or j<0 or j>=len(lines[i]):
+                continue
+            if (not lines[i][j].isdigit()) and (lines[i][j] != "."):
+                return True
+    return False
+
+with open("input.txt", "r") as f:
+    nums=[]
+    sum=0
+    lines = f.readlines()
+    lines = [line.strip() for line in lines]
+    for i in range(len(lines)):
+        n=1
+        j=0
+        while j<len(lines[i]):
+            n=1
+            if lines[i][j].isdigit():
+                while lines[i][j+n].isdigit():
+                    n+=1
+                    if j+n>=len(lines[i]):
+                        break
+                if adjacent_symbol_present(i,j,n,lines):
+                    nums.append(int(lines[i][j:j+n]))
+                    sum += int(lines[i][j:j+n])
+            j+=n
+
+print(sum)
